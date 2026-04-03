e2b83f90da965519e3f57e213995a93af87c50fa
SkinnyApple <alex.sun@kentshill.org>
2026-04-03 13:34:06 -0400

    Changed the drivesubsystem code to the one from 2025,(removed or the poseestimator limelight stuff)

--- subsystems/DriveSubsystem.py
+++ subsystems/DriveSubsystem.py
@@ -1,66 +1,52 @@
+#
+# Copyright (c) FIRST and other WPILib contributors.
+# Open Source Software; you can modify and/or share it under the terms of
+# the WPILib BSD license file in the root directory of this project.
+#
 import math
 import navx
-import wpilib
 from wpimath.geometry import Pose2d, Rotation2d
-from wpimath.kinematics import ChassisSpeeds, SwerveDrive4Kinematics, SwerveDrive4Odometry, SwerveModuleState
-from wpimath.estimator import SwerveDrive4PoseEstimator
+from wpimath.kinematics import ChassisSpeeds, SwerveDrive4Kinematics, SwerveDrive4Odometry, SwerveModuleState, SwerveModulePosition
 from Constants import DriveConstants
-from commands2 import Subsystem
+from commands2 import Subsystem, SwerveControllerCommand
 from subsystems.MAXSwerveModule import MAXSwerveModule
-from subsystems.LimelightSubsystem import LimelightSubsystem
-
+import Constants
+from wpimath.controller import PIDController, HolonomicDriveController, ProfiledPIDControllerRadians
 
 class DriveSubsystem(Subsystem):
-    def __init__(self, limelight=None):
-        super().__init__()
-
-        # Create MAX Swerve Modules
+    def __init__(self):
+        # Create MAXSwerveModules
         self.m_frontLeft = MAXSwerveModule(
             DriveConstants.kFrontLeftDrivingCanId,
             DriveConstants.kFrontLeftTurningCanId,
             DriveConstants.kFrontLeftChassisAngularOffset
         )
+
         self.m_frontRight = MAXSwerveModule(
             DriveConstants.kFrontRightDrivingCanId,
             DriveConstants.kFrontRightTurningCanId,
             DriveConstants.kFrontRightChassisAngularOffset
         )
+
         self.m_rearLeft = MAXSwerveModule(
             DriveConstants.kRearLeftDrivingCanId,
             DriveConstants.kRearLeftTurningCanId,
             DriveConstants.kBackLeftChassisAngularOffset
         )
+
         self.m_rearRight = MAXSwerveModule(
             DriveConstants.kRearRightDrivingCanId,
             DriveConstants.kRearRightTurningCanId,
             DriveConstants.kBackRightChassisAngularOffset
         )
 
-        # Gyro
+        # The gyro sensor
         self.m_gyro = navx.AHRS(navx.AHRS.NavXComType.kMXP_SPI)
 
-        # Limelight reference
-        self.limelight = limelight # this is instance, but if a bug apears, change this to subsystem instead.
-
-        # Pose Estimator
-        self.poseEstimator = SwerveDrive4PoseEstimator(
+        # Odometry class for tracking robot pose
+        self.m_odometry = SwerveDrive4Odometry(
             DriveConstants.kDriveKinematics,
-            Rotation2d.fromDegrees(-self.m_gyro.getAngle()),
-            [
-                self.m_frontLeft.get_position(),
-                self.m_frontRight.get_position(),
-                self.m_rearLeft.get_position(),
-                self.m_rearRight.get_position()
-            ],
-            Pose2d()
-        )
-        self.poseEstimator.setVisionMeasurementStdDevs((0.5, 0.5, math.radians(10)))  # adjust trust
-
-    # Periodic updates
-    def periodic(self):
-        # Update odometry from gyro
-        self.poseEstimator.update(
-            Rotation2d.fromDegrees(-self.m_gyro.getAngle()),
+            Rotation2d.fromDegrees(self.m_gyro.getAngle()),
             [
                 self.m_frontLeft.get_position(),
                 self.m_frontRight.get_position(),
@@ -69,21 +55,24 @@ class DriveSubsystem(Subsystem):
             ]
         )
 
-        """
-        if isinstance(self.limelight, LimelightSubsystem):
-            visionPose = self.limelight.get_field_pose()
-            if visionPose is not None:
-                self.poseEstimator.addVisionMeasurement(visionPose, wpilib.Timer.getFPGATimestamp())
-        """
+    def periodic(self):
+        # Update the odometry in the periodic block
+        self.m_odometry.update(
+            Rotation2d.fromDegrees(self.m_gyro.getAngle()),
+            [
+                self.m_frontLeft.get_position(),
+                self.m_frontRight.get_position(),
+                self.m_rearLeft.get_position(),
+                self.m_rearRight.get_position()
+            ]
+        )
 
-    # Get current estimated pose
     def getPose(self):
-        return self.poseEstimator.getEstimatedPosition()
+        return self.m_odometry.getPose()
 
-    # Reset odometry
     def resetOdometry(self, pose: Pose2d):
-        self.poseEstimator.resetPosition(
-            Rotation2d.fromDegrees(-self.m_gyro.getAngle()),
+        self.m_odometry.resetPosition(
+            Rotation2d.fromDegrees(self.m_gyro.getAngle()),
             [
                 self.m_frontLeft.get_position(),
                 self.m_frontRight.get_position(),
@@ -93,8 +82,8 @@ class DriveSubsystem(Subsystem):
             pose
         )
 
-    # Driving
-    def drive(self, xSpeed, ySpeed, rot, fieldRelative=True):
+    def drive(self, xSpeed, ySpeed, rot, fieldRelative):
+        # Convert the commanded speeds into the correct units for the drivetrain
         xSpeedDelivered = xSpeed * DriveConstants.kMaxSpeedMetersPerSecond
         ySpeedDelivered = ySpeed * DriveConstants.kMaxSpeedMetersPerSecond
         rotDelivered = rot * DriveConstants.kMaxAngularSpeed
@@ -102,7 +91,9 @@ class DriveSubsystem(Subsystem):
         if fieldRelative:
             swerveModuleStates = DriveConstants.kDriveKinematics.toSwerveModuleStates(
                 ChassisSpeeds.fromFieldRelativeSpeeds(
-                    xSpeedDelivered, ySpeedDelivered, rotDelivered,
+                    xSpeedDelivered,
+                    ySpeedDelivered,
+                    rotDelivered,
                     Rotation2d.fromDegrees(-self.m_gyro.getAngle())
                 )
             )
@@ -110,36 +101,43 @@ class DriveSubsystem(Subsystem):
             swerveModuleStates = DriveConstants.kDriveKinematics.toSwerveModuleStates(
                 ChassisSpeeds(xSpeedDelivered, ySpeedDelivered, rotDelivered)
             )
-
+        
         # Desaturate wheel speeds
         SwerveDrive4Kinematics.desaturateWheelSpeeds(swerveModuleStates, DriveConstants.kMaxSpeedMetersPerSecond)
 
-        # Apply states
+        # Set the desired state for each swerve module
         self.m_frontLeft.set_desired_state(swerveModuleStates[0])
         self.m_frontRight.set_desired_state(swerveModuleStates[1])
         self.m_rearLeft.set_desired_state(swerveModuleStates[2])
         self.m_rearRight.set_desired_state(swerveModuleStates[3])
 
-    # Set wheels in X for locking
     def setX(self):
         self.m_frontLeft.set_desired_state(SwerveModuleState(0, Rotation2d.fromDegrees(45)))
         self.m_frontRight.set_desired_state(SwerveModuleState(0, Rotation2d.fromDegrees(-45)))
         self.m_rearLeft.set_desired_state(SwerveModuleState(0, Rotation2d.fromDegrees(-45)))
         self.m_rearRight.set_desired_state(SwerveModuleState(0, Rotation2d.fromDegrees(45)))
 
-    # Reset all encoders
+    def setModuleStates(self, desiredStates):
+        # Desaturate wheel speeds
+        SwerveDrive4Kinematics.desaturateWheelSpeeds(desiredStates, DriveConstants.kMaxSpeedMetersPerSecond)
+
+        # Set the desired state for each swerve module
+        self.m_frontLeft.set_desired_state(desiredStates[0])
+        self.m_frontRight.set_desired_state(desiredStates[1])
+        self.m_rearLeft.set_desired_state(desiredStates[2])
+        self.m_rearRight.set_desired_state(desiredStates[3])
+
     def resetEncoders(self):
         self.m_frontLeft.reset_encoders()
         self.m_rearLeft.reset_encoders()
         self.m_frontRight.reset_encoders()
         self.m_rearRight.reset_encoders()
 
-    # Gyro helpers
     def zeroHeading(self):
         self.m_gyro.zeroYaw()
 
     def getHeading(self):
         return Rotation2d.fromDegrees(self.m_gyro.getAngle())
-
+    
     def getTurnRate(self):
         return self.m_gyro.getRate() * (-1.0 if DriveConstants.kGyroReversed else 1.0)
